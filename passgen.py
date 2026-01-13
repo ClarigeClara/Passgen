5f1f176259c0c2fc5876c3520859da92cd08ebfb
ClarigeClara <clara.steinholz@gmail.com>
2026-01-13 11:32:55 +0100

    Update auf Version 2.0.0

--- passgen.py
+++ passgen.py
@@ -1,26 +1,24 @@
-import random
+import secrets
+import string
 
-keygen = ["A", "a", "Ä", "ä", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "M", "m", "N", "n", "O", "o", "Ö", "ö", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "Ü", "ü", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "?", "$", "€", "%", "&"]
-print("Passgen by Codestube\n©2023-2025 Codestube, @clarigeclara\nVersion 1.1.0\nhttps://github.com/ClarigeClara/Passgen \n")
-print("Hier ist eine Liste mit 20 Passwort-Vorschlägen, die du verwenden kannst.\n")
+print("Passgen by Codestube\n©2023-2026 Codestube, @clarigeclara\nVersion 2.0.0\nhttps://github.com/ClarigeClara/Passgen \n")
+print("Hier ist eine Liste mit 50 Passwort-Vorschlägen, die du verwenden kannst.\n")
 
-print('''
-Vorschlag Nr.   Passwörter mit 50 Zeichen
--------------------------------------------------------------------------''')
-for i in range(1, 51):
-    passwd = "".join(random.choices(keygen, k=50))
-    print(f"# {i:5}         {passwd}")
+us_chars = string.ascii_letters + string.digits + "!@#$%&*()_+=-[]{};:<>,./?" 
+de_chars = us_chars + "äöüÄÖÜß§€"
 
-print('''
-Vorschlag Nr.   Passwörter mit 30 Zeichen
--------------------------------------------------------------------------''')
-for i in range(1, 51):
-    passwd = "".join(random.choices(keygen, k=30))
-    print(f"# {i:5}         {passwd}")
+def generate_list(name, char_set, length, count=50):
+    print(f"\n{name} {length} Zeichen")
+    print("-" * 75)
+    for i in range(1, count + 1):
+        # secrets.choice ist der Goldstandard für Krypto-Zufall
+        passwd = "".join(secrets.choice(char_set) for _ in range(length))
+        print(f"# {i:5}         {passwd}")
 
-print('''
-Vorschlag Nr.   Passwörter mit 20 Zeichen
--------------------------------------------------------------------------''')
-for i in range(1, 51):
-    passwd = "".join(random.choices(keygen, k=20))
-    print(f"# {i:5}         {passwd}")
+
+generate_list("Vorschlag Nr.   Passwörter mit", de_chars, 50)
+generate_list("Vorschlag Nr.   Passwörter (US-ASCII) mit", us_chars, 50)
+generate_list("Vorschlag Nr.   Passwörter mit", de_chars, 30)
+generate_list("Vorschlag Nr.   Passwörter (US-ASCII) mit", us_chars, 30)
+generate_list("Vorschlag Nr.   Passwörter mit", de_chars, 20)
+generate_list("Vorschlag Nr.   Passwörter (US-ASCII) mit", us_chars, 20)
